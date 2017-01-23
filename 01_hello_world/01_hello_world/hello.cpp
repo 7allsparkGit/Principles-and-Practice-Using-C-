972b06c6232dd850168433be8ce046492c51b5ad
allspark <allsparklearningmaterial@gmail.com>
2017-01-23 01:04:16 +0100

    PPU01(The Basic) - chapter 04 - Dril exercises solutions - task 1 - task 7

--- 01_hello_world/01_hello_world/hello.cpp
+++ 01_hello_world/01_hello_world/hello.cpp
@@ -1,6 +1,6 @@
 #include "std_lib_facilities.h" 
 
-	// simple program to exercise operators
+// simple program to exercise operators
 
 int square(int sqr) {
 	int square = 0;
@@ -34,7 +34,7 @@ bool TerminatingWithPipeWhileLoop()
 
 	cout << "First: ";
 	cin >> one;
-	
+
 	if (one != "|") {
 		cout << "Second: ";
 		cin >> two;
@@ -69,10 +69,126 @@ bool TerminatingWithPipeWhileLoop()
 	else return false;
 }
 
+void TerminatingWithPipeWhileLoopSecond()
+{
+	double val1;
+	double val2;
+	char n;
+	cout << "Enter | if you want to quit: ";
+	cin >> n;
+	double smaller = 0, bigger = 0;
+	while (n != '|')
+	{
+		cout << "Enter two intergers:\n";
+		cout << "Interger 1: ";
+		cin >> val1;
+		cout << "Interger 2: ";
+		cin >> val2;
+		//cout << val1 << '\t' << val2;
+		cout << endl;
+
+
+		if (val1 > val2) {
+			cout << "Smaller value is:  " << val2 << "\n";
+			cout << "The greater value is:  " << val1 << "\n";
+			smaller = val2;
+			bigger = val1;
+		}
+		else if (val2 > val1) {
+			cout << "Smaller value is:  " << val1 << "\n";
+			cout << "The greater value is:  " << val2 << "\n";
+			smaller = val1;
+			bigger = val2;
+		}
+		else if (val1 = val2) {
+			cout << "The numbers are equal\n";
+		}
+		cout << "The difference between the two number is: "
+			<< bigger - smaller << "\n";
+
+
+		cout << "Enter | if you want to quit: ";
+		cin >> n;
+	}
+}
+
+
+void SmallestBiggestSoFar()
+{
+	double num;
+	double smaller, bigger;
+	char n;
+	cout << "Enter | if you want to quit: ";
+	cin >> n;
+	cout << "Enter a number: \n";
+	cin >> num;
+	smaller = num;
+	bigger = num;
+
+	while (n != '|')
+	{
+		cout << "Enter a number: \n";
+		cin >> num;
+
+		if (num > bigger) {
+			bigger = num;
+			cout << "the highest so far\n";
+		}
+		else if (num < smaller) {
+			smaller = num;
+			cout << "the smallest so far\n";
+		}
+		cout << "Enter | if you want to quit: ";
+		cin >> n;
+	}
+}
+
+void ValueUnits()
+{
+	double num;
+	double smaller, bigger;
+	int numberOfValus = 0;
+	string unit = "";
+	char n;
+	cout << "Enter | if you want to quit: ";
+	cin >> n;
+	cout << "Enter a number follow by a unit like this 3.33m \n"
+		<< "only use cm, in, ft, m: ";
+	cin >> num >> unit;
+	smaller = num;
+	bigger = num;
+
+	while (n != '|')
+	{	// TODO reject inputs such as: y, yard, meter, km, gallons by explicitly printing out
+		// and reject every other non relevant input
+		// TODO check string[0] if it is "_" then string[0] = string[1]
+		// TODO case for cm, in, ft, m:
+		// TODO keep track the sum of entered values  in meters
+		// TODO keep track smallest largest
+		// TODO keep all values enterd in vector, at the end write out those, 
+		// before writing out sort them
+		cout << "Enter a number: \n";
+		cin >> num;
+
+		if (num > bigger) {
+			bigger = num;
+			cout << "the highest so far\n";
+		}
+		else if (num < smaller) {
+			smaller = num;
+			cout << "the smallest so far\n";
+		}
+		cout << "Enter | if you want to quit: ";
+		cin >> n;
+
+		numberOfValus++;
+	}
+}
+
 int main()
 {
 
-	while (TerminatingWithPipeWhileLoop()) { };
+	ValueUnits();
 
 
 	return 0;
