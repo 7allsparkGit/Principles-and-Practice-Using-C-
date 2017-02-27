1f26d2adb225a74f826f57e63d5673dfd7311b96
allspark <allsparklearningmaterial@gmail.com>
2017-02-27 23:12:50 +0100

    PPU01(The Basic) - chapter 05 - Exercises 5,6, error handling

--- 01_hello_world/01_hello_world/erros.cpp
+++ 01_hello_world/01_hello_world/erros.cpp
@@ -52,7 +52,7 @@ double Ctok(double c) // converts Celsius to Kelvin
 
 
 //##########################################################################################
-//##					Celsius to Kelvin converter - Exercises 2,3,4	 					##
+//##					Celsius to Kelvin converter - Exercises 2,3,4, 5				##
 //##########################################################################################
 //double ctok(double c) // converts Celsius to Kelvin
 //{
@@ -60,7 +60,7 @@ double Ctok(double c) // converts Celsius to Kelvin
 //	return k;
 //}
 //
-///// unkomment main Main for this, then comment it back after testing it!
+///// uncomment main Main for this, then comment it back after testing it!
 //int main()
 //{
 //	
@@ -86,7 +86,7 @@ double Ctok(double c) // converts Celsius to Kelvin
 //	return k;
 //}
 //
-///// unkomment main Main for this, then comment it back after testing it!
+///// uncomment main Main for this, then comment it back after testing it!
 //int main()
 //{
 //	double c = 0; // declare input variable
@@ -112,54 +112,201 @@ double Ctok(double c) // converts Celsius to Kelvin
 
 
 /// same program handle errors inside CtokTwo
+//class Too_low_value {};
+//
+//double CtokTwo(double c) // converts Celsius to Kelvin
+//{
+//	//double k = 0;
+//
+//	double k = c + 273.15;
+//	try {
+//		if (k < -273.15) {
+//			throw Too_low_value();
+//		}
+//	}
+//	catch (Too_low_value) {
+//		std::cout <<  "Kelvin is too low!\n"; // print out temperature
+//		error("Too low input");
+//		return 1;
+//	}
+//	return k;
+//}
+//
+//double KtoC(double kelvin) // converts Celsius to Kelvin
+//{
+//	//double k = 0;
+//	double celsius = 0;
+//	
+//	try {
+//		celsius = kelvin - 273.15;
+//		if (kelvin < -273.15) {
+//			throw Too_low_value();
+//		}
+//	}
+//	catch (Too_low_value) {
+//		std::cout << "Kelvin is too low!\n"; // print out temperature
+//		error("Too low input");
+//		return 1;
+//	}
+//	return celsius;
+//}
+//
+//
+///// uncomment main Main for this, then comment it back after testing it!
+//// main handles input error also, but it is optional
+//int main()	
+//{
+//	char KelvinOrCelsius = ' ';
+//	double k = 0;
+//	double c = 0; // declare input variable
+//				  /// abort program in case of bad input type
+//	
+//	std::cout << "Type 'k' to convert from Celsius to Kelvin or 'c' to convert from Kelvin to Celsius: \n";
+//	std::cin >> KelvinOrCelsius;
+//
+//	if (KelvinOrCelsius == 'k') 
+//	{
+//		std::cout << "\nEnter the amount you do want to convert: ";
+//		std::cin >> c; // retrieve temperature to input variable
+//	
+//		if (!std::cin) 
+//		{
+//			std::cout << "only numbers!\n"; // print out temperature
+//			error("couldn't read a double!");
+//		}
+//		if (!std::cin) cerr << "couldn't read a double!";
+//
+//		k = CtokTwo(c); // convert temperature
+//
+//		std::cout << k << " is the calculated Kelvin!";
+//	}
+//	else if (KelvinOrCelsius == 'c')
+//	{
+//		std::cout << "\nEnter the amount you do want to convert: ";
+//		std::cin >> c; // retrieve temperature to input variable
+//
+//		if (!std::cin)
+//		{
+//			std::cout << "only numbers!\n"; // print out temperature
+//			error("couldn't read a double!");
+//		}
+//		if (!std::cin) cerr << "couldn't read a double!";
+//
+//		k = KtoC(c); // convert temperature
+//
+//		std::cout << k << " is the calculated Celsius!";
+//
+//	}
+//	else std::cout << "Next time try appropriate input!";
+//	return 0;
+//}
+
+//##########################################################################################
+//##					Fahrenheit - Celsius converter - Exercises 6	 ##
+//##########################################################################################
 class Too_low_value {};
 
-double CtokTwo(double c) // converts Celsius to Kelvin
+double CelsiusToFahreinheit(double Celsius) // converts Celsius to Kelvin
 {
 	//double k = 0;
-
-	double k = c + 273.15;
+	double Fahreinheit = 0;
+	
 	try {
-		if (k < -273.15) {
+		Fahreinheit = 9.0/5 * Celsius + 32;
+		if (Fahreinheit < -459.67) {
 			throw Too_low_value();
 		}
 	}
 	catch (Too_low_value) {
-		std::cout <<  "Kelvin is too low!\n"; // print out temperature
+		std::cout << "Fahrenheit is too low!\n"; // print out temperature
 		error("Too low input");
 		return 1;
 	}
-	return k;
+	return Fahreinheit;
 }
 
-/// unkomment main Main for this, then comment it back after testing it!
-// main handles input error also, but it is optional
-int main()
+double FahreinheitToCelsius(double Fahrenheit) // converts Celsius to Kelvin
 {
-	double k = 0;
+	//double k = 0;
+	double Celsius = 0;
+
+	try {
+		Celsius = (Fahrenheit - 32) * 5/9;
+		if (Celsius < -273.15) {
+			throw Too_low_value();
+		}
+	}
+	catch (Too_low_value) {
+		std::cout << "Celsius is too low!\n"; // print out temperature
+		error("Too low input");
+		return 1;
+	}
+	return Celsius;
+}
+
+/// uncomment main Main for this, then comment it back after testing it!
+// main handles input error also, but it is optional
+void ProgramFahreinheitToCelsius()
+{
+	char FahrToCelsius = ' ';
+	double f = 0;
 	double c = 0; // declare input variable
 				  /// abort program in case of bad input type
-	std::cin >> c; // retrieve temperature to input variable
-	if (!std::cin) { 
-		std::cout << "only numbers!\n"; // print out temperature
-		error("couldn't read a double!"); 
+	
+	std::cout << "Type 'f' to convert from Celsius to Fahrenheit or 'c' to convert from Fahrenheit to Celsius: \n";
+	std::cin >> FahrToCelsius;
+
+	if (FahrToCelsius == 'f')
+	{
+		std::cout << "\nEnter the amount you do want to convert: ";
+		std::cin >> c; // retrieve temperature to input variable
+	
+		if (!std::cin) 
+		{
+			std::cout << "only numbers!\n"; // print out temperature
+			error("couldn't read a double!");
+		}
+		if (!std::cin) cerr << "couldn't read a double!";
+
+		f = CelsiusToFahreinheit(c); // convert temperature
+
+		std::cout << f << " is the calculated Fahrenheit!";
 	}
-	if (!std::cin) cerr << "couldn't read a double!";
-	
-	k = CtokTwo(c); // convert temperature
-	
-	std::cout << k << " is the calculated kelvin";
-	
-	return 0;
+	else if (FahrToCelsius == 'c')
+	{
+		std::cout << "\nEnter the amount you do want to convert: ";
+		std::cin >> f; // retrieve temperature to input variable
+
+		if (!std::cin)
+		{
+			std::cout << "only numbers!\n"; // print out temperature
+			error("couldn't read a double!");
+		}
+		if (!std::cin) cerr << "couldn't read a double!";
+
+		c = FahreinheitToCelsius(f); // convert temperature
+
+		std::cout << c << " is the calculated Celsius!";
+
+	}
+	else std::cout << "Next time try appropriate input!";
+}
+
+//##########################################################################################
+//##					 quadratic formula - Exercises 7	 ##
+//##########################################################################################
+void RootsOfQuadraticEquation(double a, double b, double c)
+{
+
 }
 
 
 //##########################################################################################
 //##						MAIN													###
 //##########################################################################################
-//int main()
-//{
-//	CelsiusToKelvin();
-//
-//	return 0;
-//}
+int main()
+{
+	ProgramFahreinheitToCelsius();
+
+	return 0;
+}
