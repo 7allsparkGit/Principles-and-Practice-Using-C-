0c2ad0a76dc540a47b1ef7dcf83f01ca2aba088d
allspark <allsparklearningmaterial@gmail.com>
2017-05-12 19:55:13 +0200

    PPU - chapter 09  - drill version 02

--- 01_hello_world/01_hello_world/chapter_9.cpp
+++ 01_hello_world/01_hello_world/chapter_9.cpp
@@ -1,16 +1,17 @@
 #include "std_lib_facilities.h"
 
 //#########################################################################
-//##					Chapter 9 - exercise 1.					###
+//##					Chapter 9 - drill 	§9.4.1			###
 //#########################################################################
 
 // simple Date (too simple?)
 
-		struct Date {
-			int y; // year
-			int m; // month in year
-			int d; // day of month
-		};
+		//struct Date {
+		//	int y; // year
+		//	int m; // month in year
+		//	int d; // day of month
+		//};
+
 		/*enum class Month
 		{
 			jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
@@ -21,7 +22,8 @@
 		//public:
 		//	// . . .
 		//	Date(int y, Month m, int d); // check for valid date and initialize
-		//	Date(); // default constructor		//	void add_day(Date& dat, int n);
+		//	Date(); // default constructor
+		//	void add_day(Date& dat, int n);
 
 		//void init_day(Date& dd, int y, int m, int d);
 		//void add_day(Date& dd, int n);
@@ -36,35 +38,85 @@
 		/*void init_day(Date& dd, int y, int m, int d) {};
 		void add_day(Date& dat, int d) {};*/
 
-		void init_day(Date& dd, int y, int m, int d)
-		{
-			if (m < 1 || m > 12 || d < 1 || d > 31)
-			{
-				error("Invalid date");
-			}
-			dd.y = y;
-			dd.m = m;
-			dd.d = d;
-		}
-		void add_day(Date& dat, int d)
-		{
-			dat.d += d;
-		}
-		ostream& operator<<(ostream& os, const Date& d)
-		{
-			return os << '(' << d.y
-				<< ',' << d.m
-				<< ',' << d.d << ')';
-		}
+//		void init_day(Date& dd, int y, int m, int d)
+//		{
+//			if (m < 1 || m > 12 || d < 1 || d > 31)
+//			{
+//				error("Invalid date");
+//			}
+//			dd.y = y;
+//			dd.m = m;
+//			dd.d = d;
+//		}
+//		void add_day(Date& dat, int d)
+//		{
+//			dat.d += d;
+//		}
+//		ostream& operator<<(ostream& os, const Date& d)
+//		{
+//			return os << '(' << d.y
+//				<< ',' << d.m
+//				<< ',' << d.d << ')';
+//		}
+//int main()
+//{
+//	try {		
+//		
+//		Date today { 1987 ,  2 ,  25 };
+//		std::cout << today;
+//		//tomorrow.add_day(today, 1);
+//		
+//
+//	}
+//	catch (exception& e) {
+//		cerr << "exception: " << e.what() << endl;
+//		char c;
+//		while (cin >> c && c != ';');
+//		return 1;
+//	}
+//	catch (...) {
+//		cerr << "exception\n";
+//		char c;
+//		while (cin >> c && c != ';');
+//		return 2;
+//	}
+//}
+//#########################################################################
+//##					Chapter 9 - drill 	§9.4.2			###
+//#########################################################################
+
+// simple Date (too simple?)
+
+struct Date {
+	int y; // year
+	int m; // month in year
+	int d; // day of month
+	Date(int y, int m, int d);
+	void add_day(Date& dd, int n);
+};
+Date::Date(int yy, int mm, int dd)
+{
+	y = yy;
+	m = mm;
+	d = dd;
+}
+void Date::add_day(Date& dat, int d)
+{
+	dat.d += d;
+}
+ostream& operator<<(ostream& os, const Date& d)
+{
+	return os << '(' << d.y
+		<< ',' << d.m
+		<< ',' << d.d << ')';
+}
 int main()
 {
-	try {		
-		
-		Date today { 1987 ,  2 ,  25 };
-		std::cout << today;
-		//tomorrow.add_day(today, 1);
-		
-
+	try {
+		Date today{ 1987 ,  2 ,  25 };
+		Date tomorrow = today;
+		tomorrow.add_day(tomorrow, 1);
+		std::cout << tomorrow;
 	}
 	catch (exception& e) {
 		cerr << "exception: " << e.what() << endl;
@@ -79,4 +131,3 @@ int main()
 		return 2;
 	}
 }
-
